c6182010c27ee984807c02977c9ce0db0bc5830a
Alex Heeb <abheeb1s@gmail.com>
2020-10-18 22:40:48 -0500

    add the stuff

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ListComponent implements OnInit {
 
